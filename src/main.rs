18c2b2f047de8192c9f9841988ff56cfc15bf649
DennisYun <2008dennisyun@gmail.com>
2023-03-31 18:32:33 +0900

    for문 이용한 FizzBuzz

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
-  let apple = 3000;
-  let banana = 2000;
-
-  println!("사과 가격: {}원", apple);
-  println!("바나나 가격: {}원", banana);
+  for i in 1..101 {
+    if i % 3 == 0 && i % 5 == 0 {
+      println!("FizzBuzz");
+    } else if i % 3 == 0 {
+      println!("Fizz");
+    } else if i % 5 == 0 {
+      println!("Buzz");
+    } else {
+      println!("{}", i);
+    }
+  }
 }
