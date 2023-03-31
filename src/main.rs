12a7df6adb0b964a9878d7cb0c2ba110351e8ef5
DennisYun <2008dennisyun@gmail.com>
2023-03-31 18:34:50 +0900

    이중 for문 이용한 구구단

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 fn main() {
-  for i in 1..101 {
-    if i % 3 == 0 && i % 5 == 0 {
-      println!("FizzBuzz");
-    } else if i % 3 == 0 {
-      println!("Fizz");
-    } else if i % 5 == 0 {
-      println!("Buzz");
-    } else {
-      println!("{}", i);
+  for i in 1..=9 {
+    for j in 1..=9 {
+      print!("{:3}", i * j);
     }
+    println!();
   }
 }
