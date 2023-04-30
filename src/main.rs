08873bb82581b4dae1d786dac52a356bb9b63c73
DennisYun <2008dennisyun@gmail.com>
2023-04-30 16:15:46 +0900

    타입 최소 최대값 구하기

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 fn main() {
-  let mut a = 1;
-  let mut b = 1;
-
-  println!("{}", a);
-  println!("{}", b);
-
-  for _ in 0..30 {
-    println!("{}", a + b);
-    let temp = a;
-    a = b;
-    b = temp + b;
-  }
+  println!("i8: {} ~ {}", i8::MIN, i8::MAX);
+  println!("i16: {} ~ {}", i16::MIN, i16::MAX);
+  println!("i32: {} ~ {}", i32::MIN, i32::MAX);
+  println!("i64: {} ~ {}", i64::MIN, i64::MAX);
+  
+  println!("u8: {} ~ {}", u8::MIN, u8::MAX);
+  println!("u16: {} ~ {}", u16::MIN, u16::MAX);
+  println!("u32: {} ~ {}", u32::MIN, u32::MAX);
+  println!("u64: {} ~ {}", u64::MIN, u64::MAX);
 }
