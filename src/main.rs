feba897a51a924db2ae125738eee3f1edb1a526b
DennisYun <2008dennisyun@gmail.com>
2023-05-20 19:15:40 +0900

    표준 입출력

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-use rand::seq::SliceRandom;
-
 fn main() {
-  // 1부터 75의 값을 벡터에 대입
-  let mut nums = Vec::new();
-  for i in 1..=75 {
-    nums.push(i);
-  }
+  let a = input("첫번째 숫자 넣어라");
+  let b = input("두번째 숫자 넣어라");
 
-  // 섞기
-  let mut rng = rand::thread_rng();
-  nums.shuffle(&mut rng);
+  println!("{}와 {}를 더한 값은 {}다.", a, b, a + b);
+}
 
-  // 카드표시
-  for i in 0..25 {
-    if i == 12 {
-      print!("  *, ");
-    } else {
-      print!("{:3}, ", nums[i]);
+fn input(prompt: &str) -> f64 {
+  println!("{}", prompt);
+
+  // 입력값 가져오기
+  let mut string = String::new();
+  std::io::stdin().read_line(&mut string).expect("입력에러");
+
+  // 공백 제거후 숫자 값으로 변환
+  return string.trim().parse::<f64>().expect("숫자 아님");
+  // expect는 에러메시지 심을 수 있음
+  // unwrap은 그냥 에러 내보냄
+
+  /* 
+    match "string".parse() {
+      Ok(v) => v, // 성공시
+      Err(_) => 0, // 실패시
     }
-    
-    if i % 5 == 4 {
-      println!();
-    }
-  }
+  */
 }
