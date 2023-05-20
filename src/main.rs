ac660fd11ce70813ebe8796522090bc06dcb54ac
DennisYun <2008dennisyun@gmail.com>
2023-05-20 18:57:48 +0900

    배열을 활용한 빙고 카드

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,25 @@
-use rand::Rng;
-
-const MAP_N: usize = 25;
-const TILES: [&str; 2] = ["⬜", "⬛"];
+use rand::seq::SliceRandom;
 
 fn main() {
+  // 1부터 75까지 담긴 배열 선언
+  let mut nums = [0; 75];
+  for i in 1..=75 {
+    nums[i - 1] = i;
+  }
+
+  // 섞기
   let mut rng = rand::thread_rng();
+  nums.shuffle(&mut rng);
 
-  let mut maze = [[0; MAP_N]; MAP_N];
-
- // 둘레 벽으로 싸기
-  for n in 0..MAP_N {
-    maze[0][n] = 1;
-    maze[n][0] = 1;
-    maze[n][MAP_N - 1] = 1;
-    maze[MAP_N - 1][n] = 1;
-  }
-
-  for y in 2..(MAP_N - 2) {
-    for x in 2..(MAP_N - 2) {
-      // 두칸마다 벽하나 설치
-      if x % 2 == 1 || y % 2 == 1 {
-        continue;
+  // 카드 표시
+  for y in 0..5 {
+    for x in 0..5 {
+      let i = y * 5 + x;
+      if i == 12 {
+        print!("  *, ");
+      } else {
+        print!("{:3}, ", nums[i]);
       }
-      maze[y][x] = 1;
-
-      // 상하좌우 중 하나를 벽으로 만들기
-      let r = rng.gen_range(0..=3);
-      match r {
-        0 => maze[y - 1][x] = 1, // 상
-        1 => maze[y + 1][x] = 1, // 하
-        2 => maze[y][x - 1] = 1, // 좌
-        3 => maze[y][x + 1] = 1, // 우
-        _ => {}
-      };
-    }
-  }
-
-  // 미로 출력
-  
-  for y in 0..MAP_N {
-    for x in 0..MAP_N {
-      print!("{}", TILES[maze[y][x]]);
     }
     println!();
   }
