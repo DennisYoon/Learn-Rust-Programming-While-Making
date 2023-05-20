d66dce6dd36afb17bb9cd040260b90398c2013e5
DennisYun <2008dennisyun@gmail.com>
2023-05-20 19:04:20 +0900

    vec 타입을 활용한 빙고 카드

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 use rand::seq::SliceRandom;
 
 fn main() {
-  // 1부터 75까지 담긴 배열 선언
-  let mut nums = [0; 75];
+  // 1부터 75의 값을 벡터에 대입
+  let mut nums = Vec::new();
   for i in 1..=75 {
-    nums[i - 1] = i;
+    nums.push(i);
   }
 
   // 섞기
   let mut rng = rand::thread_rng();
   nums.shuffle(&mut rng);
 
-  // 카드 표시
-  for y in 0..5 {
-    for x in 0..5 {
-      let i = y * 5 + x;
-      if i == 12 {
-        print!("  *, ");
-      } else {
-        print!("{:3}, ", nums[i]);
-      }
+  // 카드표시
+  for i in 0..25 {
+    if i == 12 {
+      print!("  *, ");
+    } else {
+      print!("{:3}, ", nums[i]);
     }
-    println!();
+    
+    if i % 5 == 4 {
+      println!();
+    }
   }
 }
