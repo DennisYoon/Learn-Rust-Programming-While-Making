e6412f9f17995c6a88e45591e8f942e4c5b5acbb
DennisYun <2008dennisyun@gmail.com>
2023-05-20 19:27:36 +0900

    HashMap을 이용한 설문집계

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
+use std::collections::HashMap;
+
 fn main() {
-  let a = input("첫번째 숫자 넣어라");
-  let b = input("두번째 숫자 넣어라");
+  let data = "c,c,a,a,a,b,b,b,c,b,c,b,a,c,c,b,c,c,c";
+  
+  let mut map = HashMap::new();
+  map.insert("a", 0);
+  map.insert("b", 0);
+  map.insert("c", 0);
 
-  println!("{}와 {}를 더한 값은 {}다.", a, b, a + b);
-}
+  for char in data.split(",") {
+    map.insert(char, map[char] + 1);
+  }
 
-fn input(prompt: &str) -> f64 {
-  println!("{}", prompt);
-
-  // 입력값 가져오기
-  let mut string = String::new();
-  std::io::stdin().read_line(&mut string).expect("입력에러");
-
-  // 공백 제거후 숫자 값으로 변환
-  return string.trim().parse::<f64>().expect("숫자 아님");
-  // expect는 에러메시지 심을 수 있음
-  // unwrap은 그냥 에러 내보냄
-
-  /* 
-    match "string".parse() {
-      Ok(v) => v, // 성공시
-      Err(_) => 0, // 실패시
-    }
-  */
+  // 결과표시
+  for k in "a,b,c".split(",") {
+    println!("{}: {:>2}", k, map[k]);
+    // map[..]이 없으면 패닉이 일어남. map.get(..)이용하면 해결
+    // map.get()의 return값은 Option타입임
+  }
 }
