e1bec46efc39c1eb35cf801e3487196f56b98863
DennisYun <2008dennisyun@gmail.com>
2023-05-21 17:00:42 +0900

    fs로 파일 읽기로 한영 사전

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
-use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
-  // 현재 path는 src가 아님. ../src임
-  let a = "src/a.txt";
-  let b = "src/b.txt";
+  // 사전 파일 path 지정
+  let dic_file = "src/dict.txt";
 
-  // file 읽기
-  let file_a = fs::read_to_string(a).unwrap();
-  let file_b = fs::read_to_string(b).unwrap();
+  // 명령줄 인수 vec에 저장
+  let arg: Vec<String> = std::env::args().collect();
 
-  // 불필요한 공백 삭제
-  let file_a = file_a.trim();
-  let file_b = file_b.trim();
-
-  if file_a == file_b {
-    println!("Same");
-  } else {
-    println!("Different");
+  // 인수 1개면 안됨
+  if arg.len() < 2 {
+    println!("인수 왜 한개만 줌??");
+    return;
   }
+
+  // 인수로 받은 단어
+  let word = &arg[1];
+
+  // 사전 file(dict.txt) 열기
+  let fp = File::open(dic_file).unwrap();
+  // BufReader로 읽어들임
+  let reader = BufReader::new(fp);
+  for line in reader.lines() {
+    let line = line.unwrap();
+    if line.find(word) == None {
+      continue;
+    }
+    println!("{}", line);
+  }
+
 }
