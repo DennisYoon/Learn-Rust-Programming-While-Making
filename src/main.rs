e140a8c2c630e19cbe340b4658ebfb8a1811f8e0
DennisYun <2008dennisyun@gmail.com>
2023-05-21 17:40:48 +0900

    mut param

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-fn x2(arg: &mut i32) { // &mut는 값을 바꿀 수 있는 참조자라는 뜻
-  *arg = *arg * 2; // 역참조는 *로
+fn print2(mut value: i32) {
+  value *= 2;
+  println!("{}", value);
 }
 
 fn main() {
-  let mut v = 16;
-  x2(&mut v);
-  println!("{}", v);
+  let value = 2;
+  print2(value);
 }
