421ab5d4c78d6315694921b22f07336b821e9750
DennisYun <2008dennisyun@gmail.com>
2023-05-21 18:00:12 +0900

    슬라이스 타입

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
 fn main() {
-  let banana = ("banana", 1000);
-  let apple = ("apple", 500);
-
-  println!("banana와 apple 가격 합: {}", banana.1 + apple.1);
-  print_tuple(&banana);
-  print_tuple(&apple);
+  let s = String::from("RustProgrammingLanguage");
+  println!("{}", &s[0..2]); // 0번째부터 1번째까지의 slice
+  // slice는 기본적으로 참조자이기 때문에 소유권 없음 -> 기존 변수 계속 사용가능
+  // s[..]은 전체 슬라이스 값임
 }
-
-fn print_tuple(item: &(&str, i32)) { // 튜플 자료형도 소유권 이동함
-  println!("{}를 {}원에 구매", item.0, item.1);
-}
-
-// struct Item(&str, i32); 타입 선언하면 let banana = Item("banana", 1000); 같이 사용
-// 배열도 소유권 이동하는 친구임
