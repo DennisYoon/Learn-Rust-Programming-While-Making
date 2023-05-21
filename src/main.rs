7ee1b922bbbe39f1971cbddfdb0fd777b9726508
DennisYun <2008dennisyun@gmail.com>
2023-05-21 17:37:47 +0900

    역참조와 &mut

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-fn main() {
-  let g1 = String::from("wow world");
-  let g2 = &g1;
-  // &는 g1에 대한 '참조자'로써 g1의 값을 '빌린 것'이다. 고로 소유권이 이동하지 않는다.
-  println!("{} {}", g1, g2);
-
-  println!("{}", hello_world());
+fn x2(arg: &mut i32) { // &mut는 값을 바꿀 수 있는 참조자라는 뜻
+  *arg = *arg * 2; // 역참조는 *로
 }
 
-fn hello_world() -> &str {
-  let string = String::from("hello_world");
-  return &string;
-  // 참조자를 반환하는데 함수 scope이 끝나면 주인 string이 파기 되기 때문에 참조자를 값으로 사용할 수 없음.
+fn main() {
+  let mut v = 16;
+  x2(&mut v);
+  println!("{}", v);
 }
