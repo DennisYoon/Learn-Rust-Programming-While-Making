ff43e4ed7159f0b9f1130eab043590e8545db33b
DennisYun <2008dennisyun@gmail.com>
2023-05-21 17:56:23 +0900

    튜플 자료형

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-fn print2(mut value: i32) {
-  value *= 2;
-  println!("{}", value);
+fn main() {
+  let banana = ("banana", 1000);
+  let apple = ("apple", 500);
+
+  println!("banana와 apple 가격 합: {}", banana.1 + apple.1);
+  print_tuple(&banana);
+  print_tuple(&apple);
 }
 
-fn main() {
-  let value = 2;
-  print2(value);
+fn print_tuple(item: &(&str, i32)) { // 튜플 자료형도 소유권 이동함
+  println!("{}를 {}원에 구매", item.0, item.1);
 }
+
+// struct Item(&str, i32); 타입 선언하면 let banana = Item("banana", 1000); 같이 사용
+// 배열도 소유권 이동하는 친구임
