9db74897d5b8bbcfee521147d732a191af6275f0
DennisYun <2008dennisyun@gmail.com>
2023-05-21 17:28:10 +0900

    소유권 시스템2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 fn main() {
-  {
-    let g1 = String::from("hello world");
-    let g2 = g1; // String객체의 소유권이 g1에서 g2로 이동
-    
-    println!("{}", g1); // 소유권이 이동했기에 g1 사용 불가
-    println!("{}", g2);
-  }
-  
+  let g1 = String::from("wow world");
+  let g2 = g1.clone(); // 원시 타입이 아닌 타입(String, Structure)은 clone해주면 복사시켜 소유권 이동 안함
 
-  {
-    {
-      let g = String::from("hey world");
-    } // g의 scope가 끝남 -> 파기
-    println!("{}", g); // g가 파기되었기에 사용 불가
-  }
+  println!("{} {}", g1, g2);
 }
+
+// 원시 타입에게는 소유권 시스템이 적용되지 않음
+// 원시 타입은 stack 메모리에 저장되기 때문에 값을 복사(copy)함
+
+// 원시 타입 외의 타입에게는 소유권 시스템이 적용됨
+// 이 타입들은 heap 메모리에 저장되어 값을 이동(move)시킴
+
+// 단 원시 타입이 아니더라도 Copy trait으로 구현한 타입이면 데이터 복사함(원시 타입은 모두 Copy Trait)
