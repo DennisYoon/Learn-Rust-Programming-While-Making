6f6c75a9945afa53ccd0ae3998be418b5efd5c08
DennisYun <2008dennisyun@gmail.com>
2023-05-21 17:35:07 +0900

    빌림과 참조

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
   let g1 = String::from("wow world");
-  let g2 = g1.clone(); // 원시 타입이 아닌 타입(String, Structure)은 clone해주면 복사시켜 소유권 이동 안함
-
+  let g2 = &g1;
+  // &는 g1에 대한 '참조자'로써 g1의 값을 '빌린 것'이다. 고로 소유권이 이동하지 않는다.
   println!("{} {}", g1, g2);
+
+  println!("{}", hello_world());
 }
 
-// 원시 타입에게는 소유권 시스템이 적용되지 않음
-// 원시 타입은 stack 메모리에 저장되기 때문에 값을 복사(copy)함
-
-// 원시 타입 외의 타입에게는 소유권 시스템이 적용됨
-// 이 타입들은 heap 메모리에 저장되어 값을 이동(move)시킴
-
-// 단 원시 타입이 아니더라도 Copy trait으로 구현한 타입이면 데이터 복사함(원시 타입은 모두 Copy Trait)
+fn hello_world() -> &str {
+  let string = String::from("hello_world");
+  return &string;
+  // 참조자를 반환하는데 함수 scope이 끝나면 주인 string이 파기 되기 때문에 참조자를 값으로 사용할 수 없음.
+}
